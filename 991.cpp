ba2b3789c690ce7afd7cf0479516d912a8f3a676
Devansh Singh Rathore <rathoredevansh08@gmail.com>
2019-06-25 01:28:46 +0530

    important

--- 991.cpp
+++ 991.cpp
@@ -1,4 +1,28 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+
+int main(){
+    int n,i=0;
+    int sol[]={1,1,2,5,14,42,132,429,1430,4862,16796};
+   while(scanf("%d",&n)==1){
+       if(i==1)
+            printf("\n");
+        printf("%d\n",sol[n]);
+        i=1;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/* #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef vector <int> vi;
@@ -19,4 +43,4 @@ int main(){
         cout << cat[n] << "\n";
     }
     return 0;
-}
+}*/
